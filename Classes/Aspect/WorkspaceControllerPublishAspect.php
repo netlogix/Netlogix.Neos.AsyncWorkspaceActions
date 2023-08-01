44920198493cc92d9f0ee0c8f6b7c01b0423a50f
Lars Lauger <lars.lauger@netlogix.de>
2023-08-01 14:50:36 +0200

    BUGFIX: Don't try to publish non-existing nodes

--- Classes/Aspect/WorkspaceControllerPublishAspect.php
+++ Classes/Aspect/WorkspaceControllerPublishAspect.php
@@ -10,7 +10,11 @@ use Neos\Flow\Core\Bootstrap;
 use Neos\Flow\Http\HttpRequestHandlerInterface;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
 use Neos\Flow\Utility\Algorithms;
+use Neos\Neos\Ui\ContentRepository\Service\NodeService;
 use Neos\Neos\Ui\Controller\BackendServiceController;
+use Neos\Neos\Ui\Domain\Model\Feedback\Messages\Success;
+use Neos\Neos\Ui\Domain\Model\FeedbackCollection;
+use Neos\Utility\ObjectAccess;
 use Netlogix\Neos\AsyncWorkspaceActions\Domain\Model\Job;
 use Netlogix\Neos\AsyncWorkspaceActions\Domain\Repository\JobRepository;
 use Netlogix\Neos\AsyncWorkspaceActions\Job\Workspace\Publish;
@@ -48,6 +52,18 @@ class WorkspaceControllerPublishAspect
     protected $bootstrap;
 
     /**
+     * @var NodeService
+     * @Flow\Inject
+     */
+    protected $nodeService;
+
+    /**
+     * @var FeedbackCollection
+     * @Flow\Inject
+     */
+    protected $feedbackCollection;
+
+    /**
      * @var int
      * @Flow\InjectConfiguration(path="nodeThreshold")
      */
@@ -64,7 +80,18 @@ class WorkspaceControllerPublishAspect
         $controller = $joinPoint->getProxy();
         assert($controller instanceof BackendServiceController);
 
-        $nodeContextPaths = $joinPoint->getMethodArgument('nodeContextPaths');
+        $nodeContextPaths = $this->filterExistingContextPaths($joinPoint->getMethodArgument('nodeContextPaths'));
+        if (count($nodeContextPaths) === 0) {
+            $success = new Success();
+            $success->setMessage('No nodes to publish');
+            $this->feedbackCollection->add($success);
+
+            $view = ObjectAccess::getProperty($controller, 'view', true);
+            $view->assign('value', $this->feedbackCollection);
+
+            return;
+        }
+
         $requestHandler = $this->bootstrap->getActiveRequestHandler();
 
         if (!($requestHandler instanceof HttpRequestHandlerInterface) || count($nodeContextPaths) < $this->threshold) {
@@ -90,6 +117,11 @@ class WorkspaceControllerPublishAspect
         $this->jobManager->queue(self::QUEUE_NAME, $publish);
 
         $this->redirectToPoll($controller, $job);
+    }
+
+    private function filterExistingContextPaths(array $contextPaths): array
+    {
+        return array_values(array_filter($contextPaths, fn (string $contextPath) => $this->nodeService->getNodeFromContextPath($contextPath, null, null, true) !== null));
     }
 
     private function redirectToPoll(BackendServiceController $controller, Job $job): void
